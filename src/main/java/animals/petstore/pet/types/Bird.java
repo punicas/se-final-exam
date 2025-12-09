d2b89bf0217255b0b35be7578ce8f48868eebea9
punicas <punicashrestha@gmail.com>
2025-12-08 23:33:50 -0500

    Refactor Bird class to implement Pet interface

--- src/main/java/animals/petstore/pet/types/Bird.java
+++ src/main/java/animals/petstore/pet/types/Bird.java
@@ -1,39 +1,102 @@
 package animals.petstore.pet.types;
 
 import animals.AnimalType;
+import animals.petstore.pet.Pet;
 import animals.petstore.pet.attributes.Breed;
 import animals.petstore.pet.attributes.Gender;
 import animals.petstore.pet.attributes.PetType;
 import animals.petstore.pet.attributes.Skin;
+
 import java.math.BigDecimal;
 
-/**
- * Bird implements Pet via PetImpl. Uses existing enums only.
- * Skin is UNKNOWN (no FEATHERS enum), birds have 2 legs.
- */
-public class Bird extends PetImpl {
+public class Bird extends Pet implements PetImpl {
 
+    /* Properties */
+    private int numberOfLegs;
+    private Breed breed;
+
+    /**
+     * Constructor
+     */
     public Bird(AnimalType animalType,
-                Skin skin,                // pass Skin.UNKNOWN in practice
+                Skin skinType,
+                Gender gender,
+                Breed breed)
+    {
+        this(animalType, skinType, gender, breed, new BigDecimal(0));
+    }
+
+    /**
+     * Constructor
+     */
+    public Bird(AnimalType animalType,
+                Skin skinType,
+                Gender gender,
+                Breed breed,
+                BigDecimal cost)
+    {
+        this(animalType, skinType, gender, breed, cost, 0);
+    }
+
+    /**
+     * Constructor (MAIN)
+     */
+    public Bird(AnimalType animalType,
+                Skin skinType,
                 Gender gender,
                 Breed breed,
                 BigDecimal cost,
-                int petStoreId) {
-        super(animalType, skin, gender, PetType.BIRD, breed, cost, petStoreId);
-        setLegs(2);
-        // Birds don't have a defined skin type in this model; force UNKNOWN to be safe.
-        setSkin(Skin.UNKNOWN);
+                int petStoreId)
+    {
+        super(PetType.BIRD, cost, gender, petStoreId);
+        super.skinType = skinType;
+        super.animalType = animalType;
+        this.numberOfLegs = 2;     // Birds have 2 legs
+        this.breed = breed;
     }
 
-    @Override
+    /** Bird hypoallergenic check (optional like Dog/Cat) */
+    public String birdHypoallergenic() {
+        return super.petHypoallergenic(this.skinType).replaceAll("pet", "bird");
+    }
+
+    /** Bird speak method (mirrors Cat & Dog pattern) */
     public String speak() {
-        // mild variation to exercise a branch
-        return getAnimalType() == AnimalType.DOMESTIC ? "tweet tweet!" : "screech!";
+        switch (this.animalType) {
+            case DOMESTIC:
+                return "The bird goes tweet! tweet!";
+            case WILD:
+                return "The bird goes screech! screech!";
+            default:
+                return "The bird goes " + super.getPetType().speak + "! " + super.getPetType().speak + "!";
+        }
+    }
+
+    public int getNumberOfLegs() {
+        return numberOfLegs;
+    }
+
+    public void setNumberOfLegs(int numberOfLegs) {
+        this.numberOfLegs = numberOfLegs;
+    }
+
+    /** Required by PetImpl */
+    @Override
+    public Breed getBreed() {
+        return this.breed;
+    }
+
+    public AnimalType getAnimalType() {
+        return this.animalType;
     }
 
     @Override
-    public boolean isHypoAllergenic() {
-        // keep it simple; no special-casing by breed in this project
-        return false;
+    public String toString() {
+        return super.toString() +
+                "The bird is " + this.animalType + "!\n" +
+                "The bird breed is " + this.getBreed() + "!\n" +
+                this.birdHypoallergenic() + "!\n" +
+                this.speak() + "\n" +
+                "Birds have " + this.numberOfLegs + " legs!";
     }
 }
